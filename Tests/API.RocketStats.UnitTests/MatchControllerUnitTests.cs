1fb033357deaa9cbd056b74b49cece07593186c3
Ryley Banick <bubba95x+Github@protonmail.com>
2021-01-16 17:22:12 -0800

    Updated the player controller unittests

--- Tests/API.RocketStats.UnitTests/MatchControllerUnitTests.cs
+++ Tests/API.RocketStats.UnitTests/MatchControllerUnitTests.cs
@@ -31,24 +31,14 @@ namespace API.RocketStats.UnitTests
         }
 
         #region Helpers
-        private static bool AreEqualModelAndResponseDto(MatchModel model, MatchResponseDto dto)
-        {
-            return model.ID == dto.ID
-                && model.MatchDate == dto.MatchDate
-                && model.GameMode == dto.GameMode;
-        }
-
-        private static bool AreEqualRequestAndResponseDto(MatchRequestDto model, MatchResponseDto dto)
-        {
-            return model.MatchDate == dto.MatchDate
-                && model.GameMode == dto.GameMode;
-        }
-
         private static Faker<MatchModel> SetupMatchModelFaker()
         {
             return new Faker<MatchModel>()
                 .StrictMode(false) // If enabled you must have a rule for each property
-                .RuleFor(o => o.MatchDate, f => f.Date.Past());
+                .RuleFor(o => o.MatchDate, f => f.Date.Past())
+                .RuleFor(o => o.GameMode, f => f.Name.FullName())
+                .RuleFor(o => o.DateCreatedUTC, f => f.Date.Past())
+                .RuleFor(o => o.DateModifiedUTC, f => f.Date.Past());
         }
 
         private static Faker<MatchRequestDto> SetupMatchRequestDtoFaker()
@@ -58,17 +48,37 @@ namespace API.RocketStats.UnitTests
                 .RuleFor(o => o.MatchDate, f => f.Date.Past())
                 .RuleFor(o => o.GameMode, f => f.Name.FullName());
         }
+
+        private static bool AreEqualModelAndResponseDto(MatchModel model, MatchResponseDto dto)
+        {
+            return model.ID == dto.ID
+                && model.MatchDate == dto.MatchDate
+                && model.GameMode == dto.GameMode
+                && model.DateCreatedUTC == dto.DateCreatedUTC
+                && model.DateModifiedUTC == dto.DateModifiedUTC;
+        }
+
+        private static bool AreEqualRequestAndResponseDto(MatchRequestDto request, MatchResponseDto response)
+        {
+            return request.MatchDate == response.MatchDate
+                && request.GameMode == response.GameMode;
+        }
+
+        
         #endregion
 
         [Fact]
         public async Task GetAsync_HappyPath()
         {
+            // Arrange
             var matchModel = testModelMatches.Generate();
             var requestId = matchModel.ID;
             mockMatchService.Setup(service => service.GetAsync(requestId)).ReturnsAsync(matchModel);
 
+            // Act
             var response = await matchController.GetAsync(requestId);
 
+            // Assert
             mockMatchService.Verify(service => service.GetAsync(requestId), Times.Once);
             Assert.True(AreEqualModelAndResponseDto(matchModel, response));
         }
@@ -76,17 +86,24 @@ namespace API.RocketStats.UnitTests
         [Fact]
         public async Task AddAsync_HappyPath()
         {
+            // Arrange
             var matchRequestDto = testRequestDtoMatches.Generate();
-            var matchModel = mapper.Map<MatchModel>(matchRequestDto);
             var matchModelResult = mapper.Map<MatchModel>(matchRequestDto);
             var id = Guid.NewGuid();
+            var utcNow = DateTime.UtcNow;
             matchModelResult.ID = id;
+            matchModelResult.DateCreatedUTC = utcNow;
+            matchModelResult.DateModifiedUTC = utcNow;
             mockMatchService.Setup(service => service.AddAsync(It.IsAny<MatchModel>())).ReturnsAsync(matchModelResult);
 
+            // Act
             var response = await matchController.AddAsync(matchRequestDto);
 
+            // Assert
             Assert.True(AreEqualRequestAndResponseDto(matchRequestDto, response));
             Assert.Equal(id, response.ID);
+            Assert.Equal(utcNow, response.DateCreatedUTC);
+            Assert.Equal(utcNow, response.DateModifiedUTC);
             mockMatchService.Verify(service => service.AddAsync(It.IsAny<MatchModel>()), Times.Once);
         }
     }
